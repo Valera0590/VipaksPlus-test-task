1f6d6e77c455fd1945e4450aafdb3cb87f44f8a0
Valeriy <valerka.kondratenkov@mail.ru>
2022-07-19 06:37:24 +0500

    Оптимизирую код
    Вместо двух одинаковых файлов динамической библиотеки, оставлен один на CMake, генерирующий, как для Windows, так и для Linux соответствующую DLL; Код на C# теперь не зависит от платформы.

---
+++ FiguresSquareApp/FiguresSquareApp/Figures.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace FiguresSquare
+{
+    //класс прямоугольник
+    public class Rectangle
+    {
+        [DllImport("FiguresSquareSharedLibrary", CallingConvention = CallingConvention.Cdecl)]
+        static extern IntPtr CreateRectangle(double a, double b);
+
+        [DllImport("FiguresSquareSharedLibrary", CallingConvention = CallingConvention.Cdecl)]
+        static extern double SquareRectangle(IntPtr rect);
+        IntPtr rectangle;
+
+        public Rectangle(double a, double b)
+        {
+            rectangle = CreateRectangle(a, b);
+        }
+        //метод для вычисления площади
+        public double Square()
+        {
+            return SquareRectangle(rectangle);
+        }
+    }
+
+
+    //класс треугольника
+    public class Triangle
+    {
+        [DllImport("FiguresSquareSharedLibrary", CallingConvention = CallingConvention.Cdecl)]
+        static extern IntPtr CreateTriangle(double a, double h);
+
+        [DllImport("FiguresSquareSharedLibrary", CallingConvention = CallingConvention.Cdecl)]
+        static extern IntPtr CreateTriangleHeron(double a, double b, double c);
+
+        [DllImport("FiguresSquareSharedLibrary", CallingConvention = CallingConvention.Cdecl)]
+        static extern double SquareTriangle(IntPtr tr);
+        IntPtr triangle;
+        public Triangle(double a, double h)
+        {
+            triangle = CreateTriangle(a, h);
+        }
+        public Triangle(double a, double b, double c)
+        {
+            triangle = CreateTriangleHeron(a, b, c);
+        }
+        //метод для вычисления площади
+        public double Square()
+        {
+            return SquareTriangle(triangle);
+        }
+    }
+}
---
+++ FiguresSquareApp/FiguresSquareApp/MainApp.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FiguresSquare;
+
+namespace FiguresSquareApp
+{
+    internal class MainApp
+    {
+        static void Main(string[] args)
+        {
+            char variantMenu;
+            do
+            {
+                double side_1, side_2, side_3, base_tr, height;
+                Console.WriteLine("\nВыберите, какую фигуру хотите создать и вычислить для неё площадь:\n" +
+                    "1. Прямоугольник\n" +
+                    "2. Треугольник\n" +
+                    "3. Треугольник с помощью формулы Герона\n" +
+                    "4. Выход\n");
+                if (!char.TryParse(Console.ReadLine(), out variantMenu))
+                {
+                    Console.WriteLine("Введенный символ некорректен!\n");
+                    continue;
+                }
+
+                switch (variantMenu)
+                {
+                    case '1':   //прямоугольник
+                        {
+                            Console.Write("\nВведите значение для первой стороны прямоугольника ");
+                            if (!double.TryParse(Console.ReadLine(), out side_1))
+                            {
+                                side_1 = 5.9;
+                                Console.WriteLine("Введено некорректное значение для первой стороны прямоугольника,\nустанавлию длину 5,9");
+                            }
+                            Console.Write("\nВведите значение для второй стороны прямоугольника ");
+                            if (!double.TryParse(Console.ReadLine(), out side_2))
+                            {
+                                side_2 = 10.2;
+                                Console.WriteLine("Введено некорректное значение для второй стороны прямоугольника,\nустанавлию длину 10,2");
+                            }
+                            //IntPtr rectangle = CreateRectangle(side_1, side_2);
+                            Rectangle rectangle = new Rectangle(side_1, side_2);
+                            Console.WriteLine("\nПлощадь прямоугольника с введенными значениями сторон равна: " + rectangle.Square());
+                            break;
+                        }
+                    case '2':   //треугольник с тривиальным вычислением площади
+                        {
+                            Console.Write("\nВведите длину основания треугольника ");
+                            if (!double.TryParse(Console.ReadLine(), out base_tr))
+                            {
+                                base_tr = 8.5;
+                                Console.WriteLine("Введено некорректное значение длины основания треугольника,\nустанавлию длину 8,5");
+                            }
+                            Console.Write("\nВведите длину высоты треугольника ");
+                            if (!double.TryParse(Console.ReadLine(), out height))
+                            {
+                                height = 3.7;
+                                Console.WriteLine("Введено некорректное значение высоты треугольника,\nустанавлию длину 3,7");
+                            }
+                            //IntPtr triangle = CreateTriangle(base_tr, height);
+                            Triangle triangle = new Triangle(base_tr, height);
+                            Console.WriteLine("\nПлощадь треугольника с введенными значениями сторон равна: " + triangle.Square());
+                            break;
+                        }
+                    case '3':   //треугольник с вычислением площади через формулу Герона
+                        {
+                            Console.Write("\nВведите значение для первой стороны треугольника ");
+                            if (!double.TryParse(Console.ReadLine(), out side_1))
+                            {
+                                side_1 = 2.5;
+                                Console.WriteLine("Введено некорректное значение для первой стороны треугольника,\nустанавлию длину 8,5");
+                            }
+                            Console.Write("\nВведите значение для второй стороны треугольника ");
+                            if (!double.TryParse(Console.ReadLine(), out side_2))
+                            {
+                                side_2 = 4.9;
+                                Console.WriteLine("Введено некорректное значение для второй стороны треугольника,\nустанавлию длину 3,7");
+                            }
+                            Console.Write("\nВведите значение для третьей стороны треугольника ");
+                            if (!double.TryParse(Console.ReadLine(), out side_3))
+                            {
+                                side_3 = 6.3;
+                                Console.WriteLine("Введено некорректное значение для третьей стороны треугольника,\nустанавлию длину 3,7");
+                            }
+                            //IntPtr triangleHeron = CreateTriangleHeron(side_1, side_2, side_3);
+                            Triangle triangle = new Triangle(side_1, side_2, side_3);
+                            Console.WriteLine("\nПлощадь треугольника с введенными значениями сторон равна: " + triangle.Square());
+                            break;
+                        }
+                    default:
+                        {
+                            if (variantMenu != '4') Console.WriteLine("Введенный символ некорректен!\n");
+                            break;
+                        }
+                }
+
+            } while (variantMenu != '4');
+
+            Console.Write("\nВы вышли, для закрытия этого окна нажмите любую кнопку...");
+            Console.ReadKey();
+        }
+    }
+
+}
